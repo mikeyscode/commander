04afbf7adb524bc66d3d17c003c338ffe837ad19
Mikey Leah <michael.leah@purplewifi.com>
2018-05-23 10:36:22 +0100

    Added unit tests for commander, bringing it to 100% coverage

---
+++ command_test.go
@@ -0,0 +1,51 @@
+package commander_test
+
+import (
+	"testing"
+
+	"github.com/mikeyscode/commander"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAddToConsole(t *testing.T) {
+	c := commander.Console{}
+	c.Add("foo bar")
+
+	assert.NotEmpty(t, c.Commands)
+	assert.Contains(t, c.Commands, "foo bar")
+}
+
+func TestRunConsoleWithSingleCommand(t *testing.T) {
+	c := commander.Console{}
+	c.Add("echo 'Hello'")
+
+	res, err := c.Run()
+	if err != nil {
+		t.Errorf("run failed %s", err)
+	}
+
+	assert.Contains(t, string(res), "Hello")
+}
+
+func TestRunConsoleWithMultipleCommands(t *testing.T) {
+	c := commander.Console{}
+	c.Add("echo 'Hello'")
+	c.Add("echo 'World'")
+
+	res, err := c.Run()
+	if err != nil {
+		t.Errorf("run failed %s", err)
+	}
+
+	assert.Contains(t, string(res), "Hello")
+	assert.Contains(t, string(res), "World")
+}
+
+func TestRunConsoleWithInvalidCommand(t *testing.T) {
+	c := commander.Console{}
+	c.Add("foo")
+
+	_, err := c.Run()
+
+	assert.NotEmpty(t, err)
+}
